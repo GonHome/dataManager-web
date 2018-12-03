f64448e56d7f990c3839e6df3e4c50e679af8ccb
GonHome <shenl@zhili-tech.com>
2018-12-03 17:44:15 +0800

    开发套件

--- src/selectots/CommonSelectors.ts
+++ src/selectots/CommonSelectors.ts
@@ -5,3 +5,4 @@ export const getHeight = (state: stateTypes) => state.environment.height;
 export const getWidth = (state: stateTypes) => state.environment.width;
 export const getLeftWidth = (state: stateTypes) => state.environment.leftWidth;
 export const getMainHeight = (state: stateTypes) => getHeight(state) - CommonConstants.HEAD_HEIGHT;
+export const getRightWidth = (state: stateTypes) => getWidth(state) - getLeftWidth(state) - 13;
